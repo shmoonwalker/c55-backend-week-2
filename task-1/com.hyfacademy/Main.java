63dc07f0d67aca29c0411489c3a5b6db28956905
Shadi <sh.moonwalker7@gmail.com>
2026-05-13 19:57:54 +0200

    Implement `PlatformService` with course, student, and mentor management features including enrolment, progress tracking, and reporting

--- task-1/com.hyfacademy/Main.java
+++ task-1/com.hyfacademy/Main.java
@@ -1,7 +1,13 @@
 package com.hyfacademy;
 
+import com.hyfacademy.service.PlatformService;
+
 public class Main {
-    static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) {
+
+        PlatformService platformService = new PlatformService();
+
+        platformService.run();
     }
 }
