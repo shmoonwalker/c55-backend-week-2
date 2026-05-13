772c3381c3156f2649b72ab7c1ef856ac8c61bad
Shadi <sh.moonwalker7@gmail.com>
2026-05-13 19:56:57 +0200

    Refactor user hierarchy: make `User` abstract, add `Mentor` and `Student` classes with roles and enrolment logic

--- task-1/com.hyfacademy/model/Mentor.java
+++ task-1/com.hyfacademy/model/Mentor.java
@@ -1,4 +1,42 @@
 package com.hyfacademy.model;
 
-public class Mentor {
+import com.hyfacademy.exception.EnrolmentException;
+
+public class Mentor extends User {
+    private final String expertise;
+    private final Course [] assignedCourses ;
+    private  int courseCount;
+    private static int counter = 1;
+
+    private static String generateId () {
+        return String.format("COA-%03d", counter++);
+    }
+
+    public Mentor (String name, String email, String expertise) {
+        super(name,email,generateId());
+        this.expertise = expertise;
+        this.assignedCourses = new Course[3];
+        this.courseCount = 0 ;
+
+    }
+    public void assignToCourse(Course course) {
+        if (courseCount >= assignedCourses.length)
+        {
+            throw new EnrolmentException(getName() + " cannot be assigned to more than 3 courses");
+        }
+        assignedCourses[courseCount] = course ;
+        courseCount ++;
+    }
+
+    public Course[] getAssignedCourses () {
+        return assignedCourses;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+    @Override
+    public String getRole() {
+        return "MENTOR";
+    }
 }
--- task-1/com.hyfacademy/model/User.java
+++ task-1/com.hyfacademy/model/User.java
@@ -1,4 +1,39 @@
 package com.hyfacademy.model;
 
-public class User {
+public abstract class User {
+    private final String name;
+    private final String email;
+    private final String userId;
+
+    public User (String name,String email,String userId) {
+
+        this.name = name;
+        this.email = email;
+        this.userId = userId;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public  String getEmail () {
+        return email;
+    }
+
+    public  String getUserId () {
+        return userId;
+    }
+
+    public abstract String getRole();
+
+    public String getSummary () {
+
+        return "[" + getRole() + "]" + name + " | " + email ;
+
+    }
+
+    @Override
+    public String toString () {
+        return  getSummary();
+    }
 }
